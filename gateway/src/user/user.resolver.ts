4d7f1f1de0336fa43077562d447c856db0924b65
gyu <cho2304@naver.com>
2021-02-14 17:11:49 +0900

    :sparkles: basic Auth Module with JWT

---
+++ gateway/src/user/user.resolver.ts
@@ -0,0 +1,52 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UserService } from './user.service';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto';
+import { EditProfileInput, EditProfileOutput } from './dtos/edit-profile.dto';
+import { Role } from '../auth/role.decorator';
+import { AuthUser } from '../auth/auth-user.decorator';
+
+@Resolver(of => User)
+export class UserResolver {
+  constructor(private readonly usersService: UserService) {}
+
+  @Mutation(returns => CreateAccountOutput)
+  createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    return this.usersService.createAccount(createAccountInput);
+  }
+
+  @Mutation(returns => LoginOutput)
+  login(@Args('input') loginInput: LoginInput) {
+    return this.usersService.login(loginInput);
+  }
+
+  @Query(returns => User)
+  @Role(['Any'])
+  me(@AuthUser() authUser: User) {
+    return authUser;
+  }
+
+  @Role(['Any'])
+  @Query(returns => UserProfileOutput)
+  userProfile(
+    @Args() userProfileInput: UserProfileInput,
+  ): Promise<UserProfileOutput> {
+    return this.usersService.findById(userProfileInput.userId);
+  }
+
+  @Role(['Any'])
+  @Mutation(returns => EditProfileOutput)
+  editProfile(
+    @AuthUser() authUser: User,
+    @Args('input') editProfileInput: EditProfileInput,
+  ): Promise<EditProfileOutput> {
+    return this.usersService.editProfile(authUser.id, editProfileInput);
+  }
+}
